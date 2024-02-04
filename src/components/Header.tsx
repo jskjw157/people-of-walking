439afb0375a7d102fd85b87ada9bb3d17b2d9fd9
johny <jskjw157@gmail.com>
2024-02-04 11:22:33 +0900

    ✨ 머리글 생성

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <header className='bg-green-600 text-white text-center p-4'>
+            <h1>산책의 민족에 오신것을 환영합니다</h1>
+        </header>
+    );
+};
+
+export default Header;
