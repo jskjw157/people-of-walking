ced1703fc9d85f37d9e67c932de403b82b544551
johny <jskjw157@gmail.com>
2024-02-04 11:22:21 +0900

    ✨ 바닥글 생성

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="bg-green-800 text-white text-center p-4">
+            <p>© 2021 산책의 민족</p>
+        </footer>
+    )
+}
+
+export default Footer;
